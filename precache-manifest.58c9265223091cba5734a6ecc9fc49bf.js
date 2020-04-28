a37f91360a549a4595cb9b053cfbbf54bea76836
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-04-28 16:29:45 -0400

    Updates

---
+++ precache-manifest.58c9265223091cba5734a6ecc9fc49bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a37d307173bad957b2035f0c001a0d",
+    "url": "/serviceChannel-Project/index.html"
+  },
+  {
+    "revision": "811a586c5910832262c5",
+    "url": "/serviceChannel-Project/static/css/main.cfb7470a.chunk.css"
+  },
+  {
+    "revision": "3506e7020197f7f3e47b",
+    "url": "/serviceChannel-Project/static/js/2.448a2d27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/serviceChannel-Project/static/js/2.448a2d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811a586c5910832262c5",
+    "url": "/serviceChannel-Project/static/js/main.bc998624.chunk.js"
+  },
+  {
+    "revision": "ba69d695da59bf2635f5",
+    "url": "/serviceChannel-Project/static/js/runtime-main.78959825.js"
+  }
+]);
